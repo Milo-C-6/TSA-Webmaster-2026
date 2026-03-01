8275f27520a5a608f7b8b0eb057cf4a787dbce66
Milo-C-6 <cummimil000@alpinesd.org>
2026-03-01 13:21:25 -0700

    Create Logo

--- src/head.js
+++ src/head.js
@@ -12,9 +12,9 @@ function createNavBar() { // Slightly useful to have it in so I can
     }
 
     const navElem = document.createElement("nav");
-    navElem.classList.add("sticky", "top-0", "bg-white/70", "dark:bg-black/70", "p-4", "flex", "justify-between", "items-center");
-    const navBarContent = `<a>Logo</a>
-    <div class="hologram flex justify-between items-center gap-15 p-1 pl-20 pr-20 text-white rounded-2xl backdrop-blur-2xl">
+    navElem.classList.add("sticky", "top-0", "bg-white/70", "dark:bg-black/70", "p-2", "flex", "justify-between", "items-center");
+    const navBarContent = `<a href="/index.html"><img alt="Logo" src="/src/media/logo.webp" class="h-10"></a>
+    <div class="hologram flex justify-between items-center gap-15 p-1 pl-20 my-1 pr-20 text-white rounded-2xl backdrop-blur-2xl">
     <!--still feeling slightly iffy about the hologram background, not sure how I would improve it though...-->
     <a href="/index.html">Home</a>
     <a href="/resources.html">Resources</a>
